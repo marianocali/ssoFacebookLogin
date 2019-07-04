c8763872b70ede0c43b58797d7b2139a7e87f2db
Mariano Cali <mariano.cali@globant.com>
2019-07-03 23:18:32 -0300

    added logout

--- src/main/java/com/example/demo/SimpleApplication.java
+++ src/main/java/com/example/demo/SimpleApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +34,9 @@ public class SimpleApplication extends WebSecurityConfigurerAdapter {
 					.antMatchers("/","/login**","/webjars/**","/error**")
 					.permitAll()
 				.anyRequest()
-					.authenticated();
+					.authenticated()
+				.and().logout().logoutSuccessUrl("/").permitAll()
+				.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 
 }
